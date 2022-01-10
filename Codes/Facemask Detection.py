de37c6538bdab5dfe872194b0f2ad82901773cea
Normina Abo <normina.abo@msugensan.edu.ph>
2022-01-10 22:30:02 +0800

    Update Facemask Detection.py

--- Codes/Facemask Detection.py
+++ Codes/Facemask Detection.py
@@ -3,17 +3,17 @@
 Created on Fri Jan  7 21:46:57 2022
 
 @author: Mia & Normina
-
-
+"""
+ 
 import cv2
 import numpy as np
 
-net = cv2.dnn.readNet('yolov3.weights', 'yolov3')
+net = cv2.dnn.readNet('Mask.weights', 'Mask.cfg')
 classes = []
-with open('coco.names', 'r') as f:
+with open('obj.names', 'r') as f:
     classes = f.read().splitlines()
 
-cap = cv2.VideoCapture('test.mp4')    
+cap = cv2.VideoCapture(0)    
 #print(classes)
 img = cv2.imread('image.jpg')
 
@@ -21,12 +21,12 @@ while True:
     _, img = cap.read()
     height, width, _ = img.shape
     
-    #blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), (0,0,0), swapRB=True, crop=False)
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), (0,0,0), swapRB=True, crop=False)
     
     
-    #for b in blob:
-        #for n, img_blob in enumerate(b):
-         #   cv2.imshow(str(n), img_blob)
+    for b in blob:
+        for n, img_blob in enumerate(b):
+           cv2.imshow(str(n), img_blob)
     
     net.setInput(blob)
     output_layer_names = net.getUnconnectedOutlayersNames()
@@ -40,7 +40,7 @@ while True:
         for detection in output:
             scores = detection[5:]
             class_ids = np.argmax(scores) 
-            confidence = scores[class_id]
+            confidence = scores[class_ids]
             if confidence > 0.5:
               center_x = int(detection[0]*width)  
               center_y = int(detection[1]*height)  
@@ -48,11 +48,11 @@ while True:
               h = int(detection[3]*height) 
               
               x = int(center_x - w/2)
-              x = int(center_y - h/2)
+              y = int(center_y - h/2)
               
               boxes.append([x, y, w, h])
               confidences.append((float(confidence)))
-              class_ids.append(class_id)
+              class_ids.append(class_ids)
     
     #print(len(boxes))
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
@@ -77,5 +77,6 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
        break
+   
 cap.release()
 cv2.destroyAllWindows()
