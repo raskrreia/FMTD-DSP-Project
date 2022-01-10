2c637f66ba027ea9f3af03c2c57fe0694cce9d54
Normina Abo <normina.abo@msugensan.edu.ph>
2022-01-10 21:05:52 +0800

    Create Facemask Detection.py

---
+++ Codes/Facemask Detection.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  7 21:46:57 2022
+
+@author: Mia & Normina
+
+
+import cv2
+import numpy as np
+
+net = cv2.dnn.readNet('yolov3.weights', 'yolov3')
+classes = []
+with open('coco.names', 'r') as f:
+    classes = f.read().splitlines()
+
+cap = cv2.VideoCapture('test.mp4')    
+#print(classes)
+img = cv2.imread('image.jpg')
+
+while True:
+    _, img = cap.read()
+    height, width, _ = img.shape
+    
+    #blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), (0,0,0), swapRB=True, crop=False)
+    
+    
+    #for b in blob:
+        #for n, img_blob in enumerate(b):
+         #   cv2.imshow(str(n), img_blob)
+    
+    net.setInput(blob)
+    output_layer_names = net.getUnconnectedOutlayersNames()
+    layerOutputs = net.forward(output_layer_names)
+            
+    boxes = []
+    confidences = []
+    class_ids = []
+    
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_ids = np.argmax(scores) 
+            confidence = scores[class_id]
+            if confidence > 0.5:
+              center_x = int(detection[0]*width)  
+              center_y = int(detection[1]*height)  
+              w = int(detection[2]*width)
+              h = int(detection[3]*height) 
+              
+              x = int(center_x - w/2)
+              x = int(center_y - h/2)
+              
+              boxes.append([x, y, w, h])
+              confidences.append((float(confidence)))
+              class_ids.append(class_id)
+    
+    #print(len(boxes))
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    #print(indexes.flatten())
+    
+    font = cv2.FONT_HERSHET_PLAIN
+    colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+    
+    # to  identify each of the object detected
+    for i in indexes.flatten():
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids])
+        confidence = str(round(confidences[i], 2))
+        color = colors[i]
+        cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+        cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+        
+    
+    
+    cv2.imshow('Image' , img)
+   # cv2.waitKey(0)
+    key = cv2.waitKey(1)
+    if key == 27:
+       break
+cap.release()
+cv2.destroyAllWindows()
