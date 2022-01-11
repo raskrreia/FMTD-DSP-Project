50c4fc027b78dd218ae75020891d6318acdb1b42
miaabbygaleflores <miaabbygale.flores@msugensan.edu.ph>
2022-01-11 17:11:27 +0800

    Detect

---
+++ final.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 17:08:13 2022
+
+@author: rinki
+"""
+
+import cv2
+import time
+
+COLORS = [(0,255,0),(255,255,0),(0,255,0),(255,0,0)]
+
+class_names = []
+with open("obj.names","r") as f:
+    class_names = [cname.strip() for cname in f.readlines()]
+    
+print(class_names)
