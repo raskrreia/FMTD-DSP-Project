79a855759aa50ac20c0744c9a63c960c9048e405
miaabbygaleflores <miaabbygale.flores@msugensan.edu.ph>
2022-01-11 19:44:53 +0800

    detect

--- final.py
+++ final.py
@@ -13,7 +13,7 @@ class_names = []
 with open("obj.names","r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
     
-print(class_names)
+#print(class_names)
 
 #   Capture Webcam
 cap = cv2.VideoCapture(0)
@@ -23,7 +23,8 @@ net = cv2.dnnreadNet("Mask.weights","Mask.cfg")
 
 #   Detect Model
 model = cv2.dnn_DetectionModel(net)
-model.setInputParams(size=(416,416, scale = 1/255))
+model.setInputParams( scale = 1/255, size = (416,416), mean = None, swapRB = None, crop = None)
+
 
 while True:
     
@@ -43,5 +44,32 @@ while True:
     for (classid, score, box) in zip(classes, scores, boxes):
         
         #   Apply Color
+        color = COLORS[int(classid) % len(COLORS)]
         
+        #   Label
+        label = f"{class_names[classid[0]]} : {score}"
+        
+        #   Box
+        cv2.rectangle(frame, box, color, 2)
+        
+        #   Box
+        cv2.putText(frame, label, (box[0], box[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    
+    #   FPS LABEL
+    fps_label = f"FPS: {round((1.0/(end - start)), 2)}"
+    
+    #   Ewan
+    cv2.putText(frame, fps_label, (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 5)
+    cv2.putText(frame, fps_label, (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 3)
+    
+    #   Frame Title
+    cv2.imshow("detections", frame)
+
+    #   Exit
+    if cv2.waitKey(1)==27:
+        break
+
+#   Super Exit
+cap.release()
+cv2.destroyAllWindows()        
     
