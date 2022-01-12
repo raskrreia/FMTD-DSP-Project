67a575dc3387c7eaca22b4531bab74adea8f44a8
Normina Abo <normina.abo@msugensan.edu.ph>
2022-01-12 17:16:56 +0800

    Update FMType_Detection.py

--- FMType_Detection.py
+++ FMType_Detection.py
@@ -1,10 +1,18 @@
 import cv2 as cv
-#import numpy
+import numpy as np
 
-Conf_threshold = 0.7
-NMS_threshold = 0.7
+Conf_threshold = 0.4
+NMS_threshold = 0.4
 
-COLORS = [(0,255,0),(255,255,0),(0,255,0),(255,0,0)]
+COLORS = [(255,0,0),(255,0,255),(0,0,255),(0,255,255)]
+
+"""
+    Color Code:
+        Red = 0,0,255
+        Violet = 0,255,0 
+        Yellow = 0,255,255
+        Blue = 255,0,0 
+"""
 
 class_name = []
 with open('MaskTypes.names','r') as f:
@@ -28,12 +36,20 @@ while True:
     
     classes, scores, boxes = model.detect(frame,Conf_threshold, NMS_threshold)
     for (classid, score, box) in zip(classes, scores, boxes):
-        color = COLORS[int(classid)%len(COLORS)]
-        label = "%s : %f" % (class_name[classid[0]], score)
+        classes = np.argmax(scores) 
+        confidence = scores[classes]
+
+        if confidence > 0.7:
+            color = COLORS[int(classid)%len(COLORS)]
+            label = "%s : %f" % (class_name[classid[0]], score)
+                
+        else:
+            color = 0,0,255
+            label = "%s : %f" % ('No Mask Detected', score)
         
         cv.rectangle(frame,box,color,1)
         cv.putText(frame,label, (box[0], box[1]-10), cv.FONT_HERSHEY_COMPLEX, 0.5,color, 2)
-    
+   
     cv.imshow('frame',frame)
     key = cv.waitKey(1)
     if key == ord('q'):
